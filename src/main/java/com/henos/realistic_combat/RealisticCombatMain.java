1d6dcba805a4f290c3eecdbf2d1ecadb600e487e
henos1029 <henos1029@gmail.com>
2022-07-18 10:51:28 +1000

    rename (again - realistic-combat -> realistic_combat)

--- src/main/java/com/henos/realistic_combat/RealisticCombatMain.java
+++ src/main/java/com/henos/realistic_combat/RealisticCombatMain.java
@@ -1,21 +1,33 @@
 package com.henos.realistic_combat;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+
 public class RealisticCombatMain implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MODID = "realistic_combat";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+	public static final Path CONFIG_FOLDER = FabricLoader.getInstance().getConfigDir().resolve(MODID);
+	public static final Path CONFIG = CONFIG_FOLDER.resolve("config.json");
+//	private static final KeyBinding kb_enable = registerKeyBinding("enable", GLFW.GLFW_KEY_UNKNOWN);
+//	private static final KeyBinding kb_cycleProfiles = registerKeyBinding("cycle_profiles", GLFW.GLFW_KEY_UNKNOWN);
+//	private static final KeyBinding kb_swapToProfile1 = registerKeyBinding("swap_to_profile1", GLFW.GLFW_KEY_UNKNOWN);
+//	private static final KeyBinding kb_swapToProfile2 = registerKeyBinding("swap_to_profile2", GLFW.GLFW_KEY_UNKNOWN);
+//	private static final KeyBinding kb_swapToProfile3 = registerKeyBinding("swap_to_profile3", GLFW.GLFW_KEY_UNKNOWN);
+
+//	private static KeyBinding registerKeyBinding(String binding, int key) {
+//		return KeyBindingHelper.registerKeyBinding(new KeyBinding("key." + MODID + "." + binding, InputUtil.Type.KEYSYM, key, "category." + MODID));
+//	}
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info(MODID + " initializing");
 	}
 }
